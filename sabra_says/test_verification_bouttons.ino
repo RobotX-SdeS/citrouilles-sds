245423f08cd9f8aa09351ba8b9bb8d8a97844de2
François-Nicolas Gitzhofer <fgitzhofer@leseminaire.ca>
2016-10-29 00:34:36 -0400

    i think i got it!!! changed most of the prog
    j'ai fait de la recherche sur comment faire clignoter la del puis attendre que quelque chose se fasse. Le problème était que je n'avais pas de boutons, alors j'ai fait un test avec un capteur d'ultrasons. J'ai donc créer une prog pour le senseur et ça a marché!!! Je me suis donc basé sur cette prog pour règler le problème. Espérons que ça marche avec le bouton aussi!

--- sabra_says/test_verification_bouttons.ino
+++ sabra_says/test_verification_bouttons.ino
@@ -2,40 +2,34 @@
 Bug Possibles/Connus
 - La led clignote toute seule (ce n'est pas parce-qu'il détecte le bouton comme étant appuyé: j'ai débranché le fil du bouton et la led continue à clignoter)
                                À Tester: Mettre un Serial.print(" "); en dessous de la détection bouton
-
 */
-const int led1 = 13;
+const int led1 = 13;//initialiser led1 dans le port 13
 const int led2 = 12;
 
-const int bo1 = 2;
+const int bo1 = 2;//initialiser bo1 dans le port 2
 const int bo2 = 3;
 
 int etatbo1 = 0;
 int etatbo2 = 0;
 
-int pans = 0; //Player answer
-int cans = 1; //Correct answer
+int pans; //Player answer// comme ce ne sont pas des constantes, pas besoin de mettre un valeur précise au début
+int cans; //Correct answer
 
 
 
 void setup() {
   
-  pinMode(led1, OUTPUT); 
+  pinMode(led1, OUTPUT);//initialiser led1 comme OUTPUT
   pinMode(led2, OUTPUT);
   
-  pinMode(bo1, INPUT);
+  pinMode(bo1, INPUT);//initialiser bo1 comme INPUT
   pinMode(bo2, INPUT);
   
-  seq1();
 }
 
-void loop() {
-  etatbo1 = digitalRead(bo1);
-  etatbo2 = digitalRead(bo2);
-  
-}
-
-
+void loop() {//début loop
+  seq1();//débuter void seq1
+}//fin loop
 
 void seq1() {
   
@@ -52,10 +46,10 @@ void seq1() {
 
 void seq1_check() {
 
- while (etatbo1 == LOW && etatbo2 == LOW) {
-  
-   
-  if (etatbo1 == HIGH) {
+etatbo1 = digitalRead(bo1);
+etatbo2 = digitalRead(bo2);
+
+if (etatbo1 == HIGH) {
     Serial.print("bo1 enfonce");
     pans = 1;
   }
@@ -65,16 +59,15 @@ void seq1_check() {
   }
   else {
     Serial.print("relache");
-    pans = 1;
+    pans = 0;
   }
-   
+   cans = 1;
   ans();
  } 
-}
+
  
  
 void ans() {
-  cans = 1;
 
   if (pans == cans) {
     digitalWrite(led1, HIGH);
@@ -95,9 +88,7 @@ void ans() {
   else {
     digitalWrite(led1, LOW);
     digitalWrite(led2, LOW);
-    ans();
+    seq1_check();
   }
   
  }
- 
- 
